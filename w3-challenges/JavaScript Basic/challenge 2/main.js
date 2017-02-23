32bfeb1a60ab1eaf703593953e04e268f5873fc2
Stephen Wright <sjw948@gmail.com>
2017-02-23 16:03:59 +0000

    challenge 2 completed

---
+++ w3-challenges/JavaScript Basic/challenge 2/main.js
@@ -0,0 +1,9 @@
+// Challenge 2
+
+// 2. Write a JavaScript program to print the contents of the current window.
+
+const printFunction = () => {
+  window.print();
+}
+
+// can be assigned to any type of event listener
