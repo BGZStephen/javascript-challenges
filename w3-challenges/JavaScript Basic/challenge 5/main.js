024d4b733a67cc53cc11a68bc2a250cbb2195595
Stephen Wright <sjw948@gmail.com>
2017-02-24 13:57:48 +0000

    challenge 5 completed

--- w3-challenges/JavaScript Basic/challenge 5/main.js
+++ w3-challenges/JavaScript Basic/challenge 5/main.js
@@ -3,18 +3,20 @@
 // 5. Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.
 
 let string = `w3resource`;
+let textHolder = document.getElementById(`text-holder`)
 
-for(let i = 0; i < 10; i++) {
+let wordRotateLoop = () => {
   setTimeout(function() {
+    let i = 0;
+    textHolder.textContent = string;
+    i ++;
     let lastLetter = string.substr(string.length - 1);
     string = string.slice(0, -1);
     string = lastLetter + string;
-    console.log(string);
-  }, 3000)
+    if (i < 100) {
+      wordRotateLoop();
+    }
+  }, 1000)
 }
 
-for(var start = 1; start < 10; start++) {
-  setTimeout(function () {
-    alert('hello');
-  }, 3000);
-}
+wordRotateLoop();
